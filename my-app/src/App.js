c2209fd78b41c7a57c3b976e5eb0bedb2a3cac1e
Balssh <george.bals25@gmail.com>
2022-11-23 20:37:45 +0200

    a working map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
-import { CssBaseline, Grid } from "@mui/material";
+import { React, useEffect } from "react";
+import { CssBaseline, Box } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
+
+import { useJsApiLoader } from "@react-google-maps/api";
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
@@ -7,16 +10,27 @@ import Map from "./components/Map/Map";
 import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 const App = () => {
+
+	const { isLoaded } = useJsApiLoader({
+		id: "google-map-script",
+        googleMapsApiKey: "AIzaSyAF--9XqeP0nbm5ZRnFeupvaOu4Ik1PR14"
+    });
+
+	useEffect(() => {
+		console.log(isLoaded);
+	  }, [isLoaded]);
+
 	return (
 		<>
 			<CssBaseline />
 			<Header />
 			<Grid container spacing={3} style={{ width: "100%" }}>
-				<Grid item xs={12} md={4}>
+				<Grid xs={12} md={4}>
 					<List />
 				</Grid>
+
 				<Grid item xs={12} md={8}>
-					<Map />
+					{ isLoaded && <Map /> }
 				</Grid>
 			</Grid>
 		</>
--- my-app/src/components/Map/Map.jsx
+++ my-app/src/components/Map/Map.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import { React, useState, useRef, useEffect, useCallback } from "react";
+import { Paper, Typography, useMediaQuery, Rating, Box } from "@mui/material";
+import { LocationOnOutlinedIcon } from "@mui/icons-material";
+import { useTheme, styled, alpha } from "@mui/material/styles";
+import { GoogleMap } from "@react-google-maps/api";
+
+const center = { lat: 45.757533, lng: 21.229066 };
 
 const Map = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const mapRef = useRef();
+
+    const onLoad = useCallback((map) => (mapRef.current = map), []);
     return (
-        <h1>Map</h1>
+        <Box
+            sx={{
+                position: "relative",
+                width: "700px",
+                height: "600px",
+                zIndex: 1,
+                "&hover": { zIndex: 2 },
+                margin: [0, 0, 0, 5],
+            }}
+        >
+            <GoogleMap
+                center={center}
+                zoom={15}
+                mapContainerStyle={{
+                    position: "relative",
+                    width: "100%",
+                    height: "100%",
+                    border: "2px solid black",
+                }}
+                onLoad={onLoad}
+            >
+
+            </GoogleMap>
+
+        </Box>
     );
 }
 
