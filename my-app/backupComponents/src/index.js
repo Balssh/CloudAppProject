eedd1df469167322672f161ff664a6ab0c67295d
Balssh <george.bals25@gmail.com>
2022-12-07 22:10:49 +0200

    backupcomponents before revamp

---
+++ my-app/backupComponents/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from 'react-auth-kit';
+import App from './App';
+import { jwtInterceptor } from './components/Helper/Interceptor';
+
+jwtInterceptor();
+
+const container = document.getElementById("root")
+createRoot(container).render(
+    // <React.StrictMode>
+
+    <AuthProvider
+        authType={"localstorage"}
+        authName={"_auth"}
+        cookieDomain={window.location.hostname}
+        cookieSecure={false}
+    >
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </AuthProvider>
+    // </React.StrictMode>
+)
