c2c24d2870ab0744a6dff82aec760cfbe95eed3d
Balssh <george.bals25@gmail.com>
2022-12-07 22:00:32 +0200

    setup for theme revamp

--- my-app/src/components/Map/Map.jsx
+++ my-app/src/components/Map/Map.jsx
@@ -5,57 +5,57 @@ import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 import MapMarker from "../MapMarker/MapMarker";
 
 const Map = ({ center, alertsList }) => {
-    const mapRef = useRef();
-    const [alerts, setAlerts] = useState(alertsList);
-    // const { isLoaded } = useJsApiLoader({
-    //     id: 'google-map-script',
-    // })
+  const mapRef = useRef();
+  const [alerts, setAlerts] = useState(alertsList);
 
-    const onLoad = useCallback((map) => {
-        const bounds = new window.google.maps.LatLngBounds(center);
-        setAlerts(alertsList);
-        alerts.map((alert, ind) => {
-            const location = new window.google.maps.LatLng(
-                alert.latitude,
-                alert.longitude,
-            );
-            bounds.extend(location);
-        });
-        map.fitBounds(bounds);
-        mapRef.current = map
-    }, []);
+  const onLoad = useCallback((map) => {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    setAlerts(alertsList);
+    alerts.map((alert, ind) => {
+      const location = new window.google.maps.LatLng(
+        alert.latitude,
+        alert.longitude
+      );
+      bounds.extend(location);
+    });
+    map.fitBounds(bounds);
+    mapRef.current = map;
+  }, []);
 
-    return true ? (
-        <GoogleMap
-            center={center}
-            zoom={15}
-            mapContainerStyle={{
-                position: "relative",
-                width: "100%",
-                height: "100%",
-                border: "2px solid black",
-            }}
-            options={{
-                maxZoom: 18,
-                minZoom: 0,
-                disableDefaultUI: true,
-            }}
-            onLoad={onLoad}
-        >
-            {alerts.map((alert, ind) => {
-                return <MapMarker key={ind} markerData={alert} />
-            })}
+  return true ? (
+    <GoogleMap
+      center={center}
+      zoom={15}
+      mapContainerStyle={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        border: "2px solid black",
+      }}
+      options={{
+        maxZoom: 18,
+        minZoom: 0,
+        disableDefaultUI: true,
+      }}
+      onLoad={onLoad}
+    >
+      {alerts.map((alert, ind) => {
+        return <MapMarker key={ind} markerData={alert} />;
+      })}
 
-            <MarkerF position={center}
-                icon={{
-                    // path: google.maps.SymbolPath.CIRCLE,
-                    url: ("https://www.svgrepo.com/show/429985/christmas-santa-claus.svg"),
-                    fillColor: '#EB00FF',
-                    scale: 7,
-                }}
-            />
-        </GoogleMap>
-    ) : <></>;
-}
+      <MarkerF
+        position={center}
+        icon={{
+          // path: google.maps.SymbolPath.CIRCLE,
+          url: "https://www.svgrepo.com/show/429985/christmas-santa-claus.svg",
+          fillColor: "#EB00FF",
+          scale: 7,
+        }}
+      />
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+};
 
 export default Map;
